d0718cad1cf21ae8505a24e7067119dc98a0dd3e
SpeedCrash100 <SpeedCrash100@yandex.ru>
2023-07-06 07:48:51 +0300

    Generic queue

---
+++ pkg/queue.go
@@ -0,0 +1,61 @@
+package pkg
+
+import "errors"
+
+var (
+	ErrQueueFull  = errors.New("queue is full")
+	ErrQueueEmpty = errors.New("queue is empty")
+)
+
+type Queue[T any] struct {
+	slice []T
+	n     int
+	start int
+	end   int
+}
+
+func NewQueue[T any](n int) Queue[T] {
+
+	return Queue[T]{
+		slice: make([]T, n),
+		n:     n,
+		start: -1,
+		end:   -1,
+	}
+}
+
+func (q *Queue[T]) IsEmpty() bool {
+	return q.start == -1 && q.end == -1
+}
+
+func (q *Queue[T]) Push(val T) error {
+	place_id := (q.end + 1) % q.n
+	if place_id == q.start {
+		return ErrQueueFull
+	}
+
+	if q.start == -1 {
+		q.start = 0
+	}
+
+	q.slice[place_id] = val
+	q.end = place_id
+
+	return nil
+}
+
+func (q *Queue[T]) Pop() (T, error) {
+	if q.IsEmpty() {
+		return q.slice[0], ErrQueueEmpty
+	}
+
+	val := q.slice[q.start]
+
+	if q.start == q.end {
+		q.start, q.end = -1, -1
+	} else {
+		q.start = (q.start + 1) % q.n
+	}
+
+	return val, nil
+}
---
+++ pkg/queue_test.go
@@ -0,0 +1,70 @@
+package pkg
+
+import "testing"
+
+func TestQueueFIFOCheck(t *testing.T) {
+	count := 100
+	q := NewQueue[int](count)
+	for i := 0; i < count; i++ {
+		if err := q.Push(i); err != nil {
+			t.Errorf("Failed to insert in queue: %v", err)
+		}
+	}
+
+	for i := 0; i < count; i++ {
+		v, err := q.Pop()
+		if err != nil {
+			t.Errorf("Failed to dequeue: %v", err)
+		}
+
+		if v != i {
+			t.Errorf("Queue is not FIFO")
+		}
+	}
+}
+
+func TestQueueOverflow(t *testing.T) {
+	count := 100
+	q := NewQueue[int](count)
+	for i := 0; i < count; i++ {
+		if err := q.Push(i); err != nil {
+			t.Errorf("Failed to insert in queue: %v", err)
+		}
+	}
+
+	if err := q.Push(0); err == nil {
+		t.Errorf("Expected overflow error")
+	}
+
+}
+
+func TestQueueUnderflow(t *testing.T) {
+	count := 100
+	q := NewQueue[int](count)
+
+	if _, err := q.Pop(); err == nil {
+		t.Errorf("Expected underrun error")
+	}
+
+	for i := 0; i < count; i++ {
+		if err := q.Push(i); err != nil {
+			t.Errorf("Failed to insert in queue: %v", err)
+		}
+	}
+
+	for i := 0; i < count; i++ {
+		v, err := q.Pop()
+		if err != nil {
+			t.Errorf("Failed to dequeue: %v", err)
+		}
+
+		if v != i {
+			t.Errorf("Queue is not FIFO")
+		}
+	}
+
+	if _, err := q.Pop(); err == nil {
+		t.Errorf("Expected underrun error")
+	}
+
+}
